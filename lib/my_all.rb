25bde6be73f2b4570a747fde7dd84e09fa14ed8f
Hannah Malm <hannahemalm616@gmail.com>
2020-12-01 00:21:21 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -4,6 +4,7 @@ def my_all?(collection)
  i = 0 
  block_return_values = [] #create an empty array 
  while i < collection.length 
+ block_return_values <<
  #when this code is run and it hits the yield line, it is going to send whatever is passed in as the argument to the block 
   yield(collection[i])
  i = i + 1  
